4801a8adadb7be7aaade62377535597253847082
Sarahgad <sarahgad91@gmail.com>
2024-01-14 11:26:24 +0200

    0

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,16 @@
-$()
+$(document).ready(function(){
+$('input.checkbox').change(function(){
+    var ameinty_dict = {};
+    if ($(this).checked()){
+        ameinty_dict[$(this).data('id')] == $(this).data('name')
+    } else {
+        delete ameinty_dict[$(this).data('id')] 
+    }
+    let lst = object.values(ameinty_dict);
+    if (lst.length > 0){
+    $('div.amenities > h4').text(lst).join(',');
+    } else {
+        $('div.amenities > h4').html('&nbsp;');
+    }
+})
+});
